3d76d98ff66acbd641427e9926e1d186f03bc45a
gsilva <gsilva.12338@gmail.com>
2025-10-02 09:15:31 -0300

    WIP

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TransactionRequest;
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TransactionController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('transaction.index');
+    }
+
+    public function create() 
+    {
+        return view('transaction.create');    
+    }
+
+    public function store(TransactionRequest $request)
+    {
+        $incommingFields = $request->validated();
+
+        try{
+            Transaction::create([
+                'decription' => data_get($incommingFields, 'description'),
+                'amount' => data_get($incommingFields, 'amount'),
+                'type' => data_get($incommingFields, 'type'),
+                'date' => data_get($incommingFields, 'date')
+            ]);
+        }catch(Exception $e){
+            Log::error($e);
+
+            return redirect()->route('transaction.index')->with('error', 'Erro ao registrar transação!');
+        }
+
+        return redirect()->route('transaction.index')->with('sucess', 'Transação registrada com sucesso!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::resource('transaction', TransactionController::class);
